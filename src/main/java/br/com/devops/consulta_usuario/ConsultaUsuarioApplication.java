de242d44de04addcb6c84a33b1c55f769f066665
Alex <alexdesouzasilva94@gmail.com>
2025-03-25 05:43:27 -0300

    first commit

---
+++ src/main/java/br/com/devops/consulta_usuario/ConsultaUsuarioApplication.java
@@ -0,0 +1,13 @@
+package br.com.devops.consulta_usuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultaUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultaUsuarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/devops/consulta_usuario/ConsultaUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.devops.consulta_usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultaUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
